ba15d0b8809093e6201b14b386d1994b37be14a3
Max <bloodmailvol1@gmail.com>
2019-10-12 17:19:00 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ShutdownPC
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.ShutdownCombobox = new MetroFramework.Controls.MetroComboBox();
+            this.ShutdownButton = new MetroFramework.Controls.MetroButton();
+            this.StopShutdownButton = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // ShutdownCombobox
+            // 
+            this.ShutdownCombobox.FormattingEnabled = true;
+            this.ShutdownCombobox.ItemHeight = 23;
+            this.ShutdownCombobox.Items.AddRange(new object[] {
+            "1 min",
+            "30 min",
+            "1 hour",
+            "2 hours",
+            "3 hours",
+            "4 hours",
+            "5 hours"});
+            this.ShutdownCombobox.Location = new System.Drawing.Point(23, 63);
+            this.ShutdownCombobox.Name = "ShutdownCombobox";
+            this.ShutdownCombobox.Size = new System.Drawing.Size(121, 29);
+            this.ShutdownCombobox.TabIndex = 0;
+            // 
+            // ShutdownButton
+            // 
+            this.ShutdownButton.Location = new System.Drawing.Point(150, 63);
+            this.ShutdownButton.Name = "ShutdownButton";
+            this.ShutdownButton.Size = new System.Drawing.Size(96, 29);
+            this.ShutdownButton.TabIndex = 1;
+            this.ShutdownButton.Text = "Shutdown";
+            this.ShutdownButton.Click += new System.EventHandler(this.ShutdownButton_Click);
+            // 
+            // StopShutdownButton
+            // 
+            this.StopShutdownButton.Location = new System.Drawing.Point(150, 98);
+            this.StopShutdownButton.Name = "StopShutdownButton";
+            this.StopShutdownButton.Size = new System.Drawing.Size(96, 29);
+            this.StopShutdownButton.TabIndex = 2;
+            this.StopShutdownButton.Text = "Stop";
+            this.StopShutdownButton.Click += new System.EventHandler(this.StopShutdownButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 151);
+            this.Controls.Add(this.StopShutdownButton);
+            this.Controls.Add(this.ShutdownButton);
+            this.Controls.Add(this.ShutdownCombobox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Style = MetroFramework.MetroColorStyle.Black;
+            this.Text = "Shutdown";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroComboBox ShutdownCombobox;
+        private MetroFramework.Controls.MetroButton ShutdownButton;
+        private MetroFramework.Controls.MetroButton StopShutdownButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using System.Diagnostics;
+
+namespace ShutdownPC
+{
+    public partial class MainForm : MetroForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ShutdownButton_Click(object sender, EventArgs e)
+        {
+            if (ShutdownCombobox.SelectedIndex == 0)
+            {
+                Process.Start("cmd", "/c shutdown -s -t 60");
+            } else if (ShutdownCombobox.SelectedIndex == 1)
+            {
+                Process.Start("cmd", "/c shutdown -s -t 1800");
+            } else if (ShutdownCombobox.SelectedIndex == 2)
+            {
+                Process.Start("cmd", "/c shutdown -s -t 3600");
+            }
+            else if (ShutdownCombobox.SelectedIndex == 3)
+            {
+                Process.Start("cmd", "/c shutdown -s -t 7200");
+            }
+            else if (ShutdownCombobox.SelectedIndex == 4)
+            {
+                Process.Start("cmd", "/c shutdown -s -t 10800");
+            }
+            else if (ShutdownCombobox.SelectedIndex == 5)
+            {
+                Process.Start("cmd", "/c shutdown -s -t 14400");
+            }
+            else if (ShutdownCombobox.SelectedIndex == 6)
+            {
+                Process.Start("cmd", "/c shutdown -s -t 18000");
+            }
+        }
+
+        private void StopShutdownButton_Click(object sender, EventArgs e)
+        {
+            Process.Start("cmd", "/c shutdown -a");
+        }
+    }
+}
